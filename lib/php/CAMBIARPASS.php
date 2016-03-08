f6d53faa2c61e8e5862c0470fe1aafb4033e69f9
lalejandro1091 <lalejandro1091@gmail.com>
2016-03-07 19:53:30 -0500

    validar
    validar ususario cuando olvida contrasena

--- lib/php/CAMBIARPASS.php
+++ lib/php/CAMBIARPASS.php
@@ -1,11 +1,26 @@
 <?php
         include("CONECTAR_DB.php");
         function cambiarPass($connect, $username, $email, $correo) {
-           
+            if(checkAvailability($connect, $username)) {
 		$query = "INSERT INTO user_data SET pass='$email'";
 		mysqli_query($connect,$query) or die("Error: " . mysqli_error());
 		return true;
+	    }
 		
+	    else {
+	        return false;
+	    }
         }
+        function checkAvailability($connect, $username) {
+		$query = "SELECT * FROM user_data WHERE user_name='$username'";
+		$result = mysqli_query($connect,$query);
+		$row = mysqli_fetch_array($result);
+		if($row['user_name'] == $username) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 
 ?>
