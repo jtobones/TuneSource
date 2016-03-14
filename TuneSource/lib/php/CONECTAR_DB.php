978d4f93064c9961c7a35491c4949cfdf5dc7b08
lalejandro1091 <lalejandro1091@gmail.com>
2016-03-13 23:33:35 -0500

    intento con script
    player con src en el script

---
+++ TuneSource/lib/php/CONECTAR_DB.php
@@ -0,0 +1,16 @@
+<?php
+	$host = "mysql.hostinger.es";
+	$username = "u944924796_root";
+	$password = "root.2016";
+	$database = "u944924796_ts";
+	$connect = mysqli_connect($host, $username, $password, $database);
+        
+        // Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+        //or die("Error while connecting to local server");
+	//mysql_select_db($database, $connect) or die("Error connecting to database");
+?>
+	
---
+++ TuneSource/lib/php/INGRESAR.php
@@ -0,0 +1,48 @@
+<?php
+	include("CONECTAR_DB.php");
+	function loginUser($connect, $username, $email) {
+		if(checkExistingUser($connect, $username, $email)) {
+			$query = "UPDATE user_data SET is_logged_in=1 WHERE user_name='$username' AND pass='$email'";
+			mysqli_query($connect, $query) or die("Error: " . mysqli_error());
+			$query = "SELECT user_id FROM user_data WHERE user_name='$username' AND pass='$email'";
+			$result = mysqli_query($connect, $query) or die("Error: ".mysqli_error());
+			$row = mysqli_fetch_array($result);
+			return $row[0];
+		}
+		else {
+			return false;
+		}
+	}
+	
+        function superUser($connect, $username, $email) {
+                 if (checkExistingUser($connect, $username, $email)){
+                    $query = "UPDATE user_data SET is_logged_in=1 WHERE user_name='$username' AND pass='$email'";
+		    mysqli_query($connect, $query) or die("Error: " . mysqli_error());
+		    $query = "SELECT type FROM user_data WHERE user_name='$username' AND pass='$email'";
+		    $result = mysqli_query($connect, $query) or die("Error: ".mysqli_error());
+		    $row = mysqli_fetch_array($result);
+		    return $row[0];
+                 }
+                 else{
+                     return false;
+                 }
+                }
+
+	function checkExistingUser($connect, $username, $email) {
+		$query = "SELECT pass FROM user_data WHERE user_name='$username'";
+		$result = mysqli_query($connect, $query) or die("Error while checking database");
+		$row = mysqli_fetch_array($result);
+		if($row[0] == $email) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	function logoutUser($connect, $id) {
+		$query = "UPDATE user_data SET is_logged_in=0 WHERE user_id='$id'";
+		mysqli_query($connect, $query) or die("Error: " . mysqli_error());
+		return true;
+	}
+?>
